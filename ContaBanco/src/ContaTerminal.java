479b3aaf62606eb904089406eb1db7a30845c43c
Tiago Bettanin <tiagobettanin29@gmail.com>
2024-04-06 20:37:09 -0300

    segudno

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.println("Por favor, digite o número da Conta !");
+        numero = sc.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência !");
+        agencia = sc.next();
+
+        System.out.println("Por favor, digite o nome do Cliente !");
+        nomeCliente = sc.next();
+
+        System.out.println("Por favor, digite o número do saldo !");
+        saldo = sc.nextDouble();
+
+        System.out.println("Olá "+ nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
+            ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");     
+        
+        sc.close();
     }
 }
